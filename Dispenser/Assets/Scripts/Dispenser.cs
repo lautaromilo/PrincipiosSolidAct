21496a92b9e0efb60740c79e0d56f6a745b5979a
MILO <lautaro_milo@hotmail.com>
2024-10-23 20:48:05 -0300

    Se modifico el script de Dispenser
    Se modifico el scipt de Dispenser par que trabajar directamente con GameObject de cada bebida. Se agrego dentro del update la detecición de teclas presionadas para que muentren un mensaje del liquido liberado al presionar las teclas "A" "S" "D" y "E"

--- Dispenser/Assets/Scripts/Dispenser.cs
+++ Dispenser/Assets/Scripts/Dispenser.cs
@@ -5,28 +5,82 @@ using System;
 
 public class Dispenser : MonoBehaviour
 {
-    [SerializeField] public Liquidos coca;
-    [SerializeField] public Liquidos agua;
-    [SerializeField] public Liquidos sprite;
-    [SerializeField] public Liquidos fanta;
+    public GameObject coca;
+    public GameObject agua;
+    public GameObject sprite;
+    public GameObject fanta;
 
-    public bool estaPresionada;
+    private GameObject soloagua;
+    private GameObject gaseosas;
 
 
     void Start()
     {
+        soloagua = GameObject.Find("SoloAgua");
+        gaseosas = GameObject.Find("Gaseosas");
 
     }
+
 
 
     void Update()
     {
-        ServirLiquido();
+       
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Coca newcoca = coca.GetComponent<Coca>();
+
+            if (newcoca != null)
+            {
+                Debug.Log("Entro a liberar Coca");
+                newcoca.Liberar(coca, gaseosas.transform);
+            }
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Agua newagua = agua.GetComponent<Agua>();
+
+            if (newagua != null)
+            {
+                Debug.Log("Entro a liberar Agua");
+
+                newagua.Liberar(agua, soloagua.transform);
+
+            }
+
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Sprite newsprite = sprite.GetComponent<Sprite>();
+          
+
+            if (newsprite != null)
+            {
+                Debug.Log("Entro a liberar Sprite");
+                newsprite.Liberar(sprite, gaseosas.transform);
+
+            }
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Fanta newfanta = fanta.GetComponent<Fanta>();
+
+            if (newfanta != null)
+            {
+                Debug.Log("Entro a liberar fanta");
+
+                newfanta.Liberar(fanta, gaseosas.transform);
+            }
+
+        }
+
+
+
     }
-
-    ServirLiquido
-    {
-
-    }
-
 }
