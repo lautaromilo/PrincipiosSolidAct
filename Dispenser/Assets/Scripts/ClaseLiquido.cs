29c69650879d9f0ce0d2bee439267b752c24577b
MILO <lautaro_milo@hotmail.com>
2024-10-23 21:52:03 -0300

    Se modifico el script de ClaseLiquido
    Se agrego el movimiento de caida a la ClaseLiquido y se agrego la funcionalidad de Desaparecer y Reactivar para los objetos dejen de verse en la escena.

--- Dispenser/Assets/Scripts/ClaseLiquido.cs
+++ Dispenser/Assets/Scripts/ClaseLiquido.cs
@@ -4,11 +4,36 @@ using UnityEngine;
 
 public abstract class ClaseLiquido : MonoBehaviour , ILiquidos
 {
-
+    public float velocidadCaida = 2f;
 
     public abstract void Liberar(GameObject agua, Transform posicion);
     
-    public abstract void Desaparecer();
+    public void MovimientoCaida()
+    {
+        transform.Translate(Vector3.down * velocidadCaida * Time.deltaTime);
+
+        if (transform.position.y < -2f)
+        {
+            Desaparecer();
+        }
+    }
+    
+    public virtual void Update()
+    {
+        MovimientoCaida();
+    }
+    
+
+    public void Desaparecer()
+    {
+        gameObject.SetActive(false);  
+    }
+
+    public void Reactivar (Transform posicion)
+    {
+        gameObject.SetActive(true); 
+        transform.position = posicion.position;
+    }
 
 
 
